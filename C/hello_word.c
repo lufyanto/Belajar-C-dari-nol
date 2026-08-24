9c207e7bbb756dbb8f398f036b024bba7263e755
Lufyanto Eka Fahrezi <lufyantoekalearning@gmail.com>
2026-08-24 17:37:44 +0700

    MENGENAL C

---
+++ C/hello_word.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hallo Dunia!\n");
+    printf("Have a good day!");
+    printf("...Ini akan kesamping terus...\n");
+    printf("Lufyanto\n\n\n");
+
+    //MEMBUAT LINE BARU
+    
+    printf("===== DATA MAHASISWA =====\n");
+    printf("NAMA = LUFYANTO\n");
+    printf("PRODI = TEKNOLOGI INFORMASI\n");
+    return 0;
+    
+}
+
+
---
+++ C/test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    char nama[50];
+    printf("Masukkan Nama anda: ");
+    scanf("%s", nama);
+    printf("Halo %s, Selamat datang \n", nama);
+    return 0;
+}
